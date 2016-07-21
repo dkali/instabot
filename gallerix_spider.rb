b31ac67d06286c428e182c838a52b108276545fe
Dmitry Kalinnikov <Dmitry Kalinnikov>
2016-07-21 21:46:06 +0300

    spider updated to catch all authors

--- gallerix_spider.rb
+++ gallerix_spider.rb
@@ -57,21 +57,26 @@ private
       puts ex.backtrace.join("\n")
       return
     end
-    # raise "Network ERROR, response code #{response.code} for #{artist_url}" if response.code != 200
 
     page = Nokogiri::HTML(response.body)
-    artist_name = page.css("td[id='n_mainleft']").css('h1')[0].text.rstrip
+    begin
+      artist_name = page.css("td[id='n_mainleft']").css('h1')[0].text.rstrip
+    rescue => ex
+      puts "WARNING: no artist discovered at #{artist_url}, skip it"
+      return
+    end
     validate_file_name(artist_name)
     puts "===#{artist_name}==="
     artist_gallery_path = File.join(self.pics_folder, artist_name)
-    Dir.mkdir(artist_gallery_path, 7777)
+    Dir.mkdir(artist_gallery_path, 7777) if not Dir.exist?(artist_gallery_path)
 
     artist_id = decode_id(artist_url)
     puts "artist_id: '#{artist_id}"
 
     pic_page_urls = []
     page.css("td[id='n_mainleft']").css('a').each do |href|
-      pic_page_urls << href['href'] if (not (href['href'] =~ /#{artist_id}/).nil?)
+      # pic_page_urls << href['href'] if (not (href['href'] =~ /#{artist_id}/).nil?)
+      pic_page_urls << href['href'] if (not (href['href'] =~ /\/storeroom\/#{artist_id}/).nil?) || (not (href['href'] =~ /^\/album\/.+\/pic\/.+$/).nil?)
     end
 
     cfg_hash = {}
@@ -83,17 +88,26 @@ private
       # break #!!!!!!!!!!!!!!!!!!!!!!!!!
     end
 
-    file = File.open(File.join(self.pics_folder, artist_name, "gallery_cfg.yaml"), 'w')
-    file.write cfg_hash.to_yaml
+    cfg_path = File.join(self.pics_folder, artist_name, "gallery_cfg.yaml")
+    old_cfg = {}
+    if File.exist?(cfg_path)
+      old_cfg = YAML::load(File.open(cfg_path))
+    end
+    cfg = old_cfg.merge(cfg_hash)
+    file = File.open(cfg_path, 'w')
+    file.write cfg.to_yaml
     file.close
     puts "==================="
   end
 
   # get artist id from a links like http://gallerix.ru/storeroom/1917286519
-  def decode_id(artist_link)
-    anchor = artist_link =~ /\/\d+$/
-    anchor = artist_link =~ /\/[A-Z]|[a-z]+$/ if anchor.nil?
-    id = artist_link[anchor+1..-1]
+  #                              or http://gallerix.ru/album/aivazovsky/pic/glrx-144686889
+  def decode_id(link)
+    anchor = link =~ /\/\d+$/
+    # anchor = link =~ /\/[A-Z]|[a-z]+$/ if anchor.nil?
+    anchor2 = link =~ /glrx-/ if anchor.nil?
+    id = link[anchor+1..-1] if not anchor.nil?
+    id = link[anchor2+5..-1] if not anchor2.nil?
     return id
   end
 
@@ -106,14 +120,13 @@ private
       puts ex.backtrace.join("\n")
       return
     end
-    # raise "Network ERROR, response code #{response.code} for #{pic_page_url}" if response.code != 200
 
     page = Nokogiri::HTML(response.body)
     pic_name = page.css("h4[itemprop='caption']").text.rstrip
-    # pic_name = validate_file_name(pic_name)
     pic_id = SecureRandom.uuid
 
     image_id = decode_id(pic_page_url)
+    puts "   image_id: #{image_id}"
 
     # #full size
     # pic_urls = []
@@ -124,14 +137,15 @@ private
     # medium size
     pic_urls = []
     page.css("td[id='n_mainleft']").css('img').each do |img|
-      pic_urls << img['src'] if (not (img['src'] =~ /\/#{image_id}/).nil?)
+      # pic_urls << img['src'] if (not (img['src'] =~ /\/#{image_id}/).nil?) || (img['class'] == "ac_pic")
+      pic_urls << img['src'] if (img['class'] == "ac_pic")
     end
 
     pic_urls.uniq!
     if pic_urls.size > 1
       puts "error detected, picture URL out of pattern, only 1 expected: #{pic_urls}"
     elsif pic_urls.empty?
-      puts "no picture found on URL #{pic_page_url}, skip it"
+      puts "WARNING: no picture found on URL #{pic_page_url}, skip it"
     else
       pic_url = pic_urls[0]
       puts "   pic_url: #{pic_url}"
@@ -144,7 +158,6 @@ private
         puts ex.backtrace.join("\n")
         return
       end
-      # raise "Network ERROR, response code #{resp.code} for #{pic_url}" if resp.code != 200
       file = File.open(File.join(artist_gallery_path, "#{pic_id}.jpg"), 'wb' ) do |output|
         output.write resp
       end
